50cbef874ef6bc556854518ec045e0ade16d9adc
Pratikk24 <65650121+Pratikk24@users.noreply.github.com>
2020-07-01 16:23:06 +0530

    Add files via upload

---
+++ Tests/Test_Cases.py
@@ -0,0 +1,72 @@
+from selenium import webdriver
+import time
+import unittest
+from Libraries.LIB_LoginPage import LoginPage
+from Libraries.LIB_HomePage import HomePage
+import HtmlTestRunner
+from Locators.locators import Locators
+
+
+
+class Bajaj_Claims_Test(unittest.TestCase):
+    URL = Locators.URL
+    username = Locators.Username
+    password = Locators.Password
+
+    @classmethod
+    def setUpClass(cls):
+        cls.driver = webdriver.Chrome(Locators.executable_path)
+        cls.driver.get(cls.URL)
+        cls.driver.implicitly_wait(10)
+        cls.driver.maximize_window()
+
+
+    def test_01_Login_valid(self):
+        driver = self.driver
+
+        login = LoginPage(driver)
+        login.enter_username(self.username)
+        login.enter_password(self.password)
+        login.click_login()
+
+        time.sleep(3)
+
+    def test_02_Claim_list_Page_Validation(self):
+        self.assertEqual(Locators.title, self.driver.title)
+
+    def test_03_Navigate_to_Document_Page(self):
+        driver = self.driver
+
+        homePage = HomePage(driver)
+        homePage.click_on_Document_list()
+
+        time.sleep(2)
+
+    def test_04_Click_on_Upload_Document(self):
+         driver = self.driver
+
+         homepage = HomePage(driver)
+         homepage.click_on_Upload_Document()
+
+         time.sleep(5)
+
+    def test_05_Logout(self):
+        driver = self.driver
+
+        homepage = HomePage(driver)
+        homepage.click_Three_Dot_Icon()
+        homepage.click_on_Logout()
+
+        time.sleep(3)
+
+
+    @classmethod
+    def tearDownClass(cls):
+        cls.driver.close()
+        cls.driver.quit()
+        print("Test Completed")
+
+
+
+if __name__ == '__main__':
+    unittest.main(testRunner=HtmlTestRunner.HTMLTestRunner(Locators.output))
