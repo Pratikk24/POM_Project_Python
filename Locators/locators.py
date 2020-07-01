6c6f902f3d5699afd9fbd251b2e7663f5568636a
Pratikk24 <65650121+Pratikk24@users.noreply.github.com>
2020-07-01 16:22:07 +0530

    Add files via upload

--- Locators/locators.py
+++ Locators/locators.py
@@ -1,10 +1,42 @@
 class Locators():
 
+    # executable_path
+    executable_path = "C:/Driver/chromedriver_win32/chromedriver.exe"
+
+    # url :
+    URL = "http://claim-portal-bajaj-dev.qa.i3systems.in/"
+
     # Login Page Objects :
     username_textbox_id = "id_username"
     password_textbox_id = "id_password"
     login_button_css_selector = "input[value=LOGIN]"
 
-    # Home Page Objects :
-    Down_Caret_Icon = "logoutCaret"
-    logout_link_linktext = "Logout"
+    # username & password
+    Username = "admin"
+    Password = "!3$y$+eMs"
+
+
+    # Home Page Objects :-
+
+    # Document Page Objects
+    Document_link_text = "DOCUMENT LIST"
+
+    # Upload Documents :
+    Upload_Document_x_path = "/html/body/app-root/section/main/div/app-document-list/section[1]/section/section[2]/section/button[1]"
+
+    # Logout :
+    Three_Dot_Icon = "//*[@id='header-main']/app-header/mat-toolbar/div[2]/ul[2]/li[3]/button/span/mat-icon"
+    logout_x_path = "//*[@id='cdk-overlay-0']/div/div/button[2]/span"
+
+    # Title
+    title = "Claim Assistant"
+
+    # Report output Path
+    output = 'C:/Users/Pratik/PycharmProjects/Claims-POM/Reports'
+
+
+
+
+
+
+
