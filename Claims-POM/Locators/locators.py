842dc0c453072984a6f55a869ade96cb2f9eae98
Pratikk24 <65650121+Pratikk24@users.noreply.github.com>
2020-07-01 12:25:02 +0530

    Add files via upload
    Tab Change Functionality & Upload Button Click

---
+++ Claims-POM/Locators/locators.py
@@ -0,0 +1,30 @@
+class Locators():
+
+    # Login Page Objects :
+    username_textbox_id = "id_username"
+    password_textbox_id = "id_password"
+    login_button_css_selector = "input[value=LOGIN]"
+
+    # Home Page Objects :-
+
+    # Document Page Objects
+    Document_link_text = "DOCUMENT LIST"
+
+    # Upload Documents :
+    Upload_Document_x_path = "/html/body/app-root/section/main/div/app-document-list/section[1]/section/section[2]/section/button[1]"
+
+    # Logout :
+    Three_Dot_Icon = "//*[@id='header-main']/app-header/mat-toolbar/div[2]/ul[2]/li[3]/button/span/mat-icon"
+    logout_x_path = "//*[@id='cdk-overlay-0']/div/div/button[2]/span"
+
+    # url :
+    URL = "http://claim-portal-bajaj-dev.qa.i3systems.in/"
+
+    # username & password
+    Username = "admin"
+    Password = "!3$y$+eMs"
+
+
+
+
+
