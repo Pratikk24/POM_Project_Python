69f926a654585cd0508bde728cacf37c1f5c35bb
vijayask <65651668+vijayask@users.noreply.github.com>
2020-07-03 14:00:41 +0530

    Add files via upload

--- Libraries/LIB_HomePage.py
+++ Libraries/LIB_HomePage.py
@@ -1,15 +1,27 @@
 from Locators.locators import Locators
 
+
+
 class HomePage():
 
     # Constructor
     def __init__(self, driver):
         self.driver = driver
 
+
         self.Three_Dot_Icon = Locators.Three_Dot_Icon
         self.Logout_x_path = Locators.logout_x_path
         self.Document_list = Locators.Document_link_text
         self.Upload_Document = Locators.Upload_Document_x_path
+        self.Claims_list = Locators.Claims_list_link_text
+        self.Select_case = Locators.Select_case
+        self.Allocation_arrow = Locators.Down_arrow_for_allocation
+        self.Qc_user_selection = Locators.Selection_of_qc_user
+        self.Qc_user_allocation = Locators.Allocation_of_qc_user
+        self.Three_Dot_Icon = Locators.Three_Dot_Icon
+        self.Logout_x_path = Locators.logout_x_path
+        self.Open_case = Locators.Open_Case_x_path
+        self.Verify_button = Locators.Verify_Button_x_path
 
     # Functions or Action Methods
 
@@ -19,11 +31,33 @@ class HomePage():
     def click_on_Upload_Document(self):
         self.driver.find_element_by_xpath(self.Upload_Document).click()
 
+    def click_on_select_case(self):
+        self.driver.find_element_by_xpath(self.Select_case).click()
+
+    def click_on_allocation_arrow(self):
+        self.driver.find_element_by_xpath(self.Allocation_arrow).click()
+
+    def click_on_qc_user_selection(self):
+        self.driver.find_element_by_xpath(self.Qc_user_selection).click()
+
+    def click_on_qc_user_allocation(self):
+        self.driver.find_element_by_xpath(self.Qc_user_allocation).click()
+
     def click_Three_Dot_Icon(self):
         self.driver.find_element_by_xpath(self.Three_Dot_Icon).click()
 
     def click_on_Logout(self):
         self.driver.find_element_by_xpath(self.Logout_x_path).click()
+
+    def click_on_Claims_list(self):
+        self.driver.find_element_by_link_text(self.Claims_list).click()
+
+    def Open_case_by_clicking(self):
+        self.driver.find_element_by_xpath(self.Open_case).click()
+
+    def Verify_case(self):
+        self.driver.find_element_by_xpath(self.Verify_button).click()
+
 
 
 
--- Tests/Test_Cases.py
+++ Tests/Test_Cases.py
@@ -3,30 +3,38 @@ import time
 import unittest
 from Libraries.LIB_LoginPage import LoginPage
 from Libraries.LIB_HomePage import HomePage
-import HtmlTestRunner
 from Locators.locators import Locators
+import HtmlTestRunner
 
 
 
-class Bajaj_Claims_Test(unittest.TestCase):
-    URL = Locators.URL
-    username = Locators.Username
-    password = Locators.Password
+class ClaimsTest(unittest.TestCase):
+    Url = Locators.URL
+    Username = Locators.username
+    Password = Locators.password
+    Qc_username = Locators.qc_username
+    Qc_password = Locators.qc_password
+
+
+
+
 
     @classmethod
     def setUpClass(cls):
-        cls.driver = webdriver.Chrome(Locators.executable_path)
-        cls.driver.get(cls.URL)
+        cls.driver = webdriver.Chrome(executable_path="C:/Users/VIJAYA/Desktop/webdrivers/chromedriver_win32/chromedriver")
+
+        cls.driver.get(cls.Url)
         cls.driver.implicitly_wait(10)
         cls.driver.maximize_window()
+
 
 
     def test_01_Login_valid(self):
         driver = self.driver
 
         login = LoginPage(driver)
-        login.enter_username(self.username)
-        login.enter_password(self.password)
+        login.enter_username(self.Username)
+        login.enter_password(self.Password)
         login.click_login()
 
         time.sleep(3)
@@ -48,9 +56,78 @@ class Bajaj_Claims_Test(unittest.TestCase):
          homepage = HomePage(driver)
          homepage.click_on_Upload_Document()
 
-         time.sleep(5)
+         time.sleep(25)
 
-    def test_05_Logout(self):
+
+
+
+    def test_05_Go_to_claim_list_page(self):
+        driver = self.driver
+
+        homepage = HomePage(driver)
+        homepage.click_on_Claims_list()
+
+        time.sleep(3)
+
+    def test_06_select_case(self):
+        driver = self.driver
+        homepage = HomePage(driver)
+        homepage.click_on_select_case()
+        time.sleep(2)
+
+    def test_07_allocation_arrow(self):
+        driver = self.driver
+        homepage = HomePage(driver)
+        homepage.click_on_allocation_arrow()
+        time.sleep(2)
+
+    def test_08_qc_user_selection(self):
+        driver = self.driver
+        homepage = HomePage(driver)
+        homepage.click_on_qc_user_selection()
+        time.sleep(2)
+
+    def test_09_qc_user_allocqtion(self):
+        driver = self.driver
+        homepage = HomePage(driver)
+        homepage.click_on_qc_user_allocation()
+        time.sleep(2)
+
+    def test_10_Logout(self):
+        driver = self.driver
+
+        homepage = HomePage(driver)
+        homepage.click_Three_Dot_Icon()
+        homepage.click_on_Logout()
+
+        time.sleep(3)
+
+    def test_11_qc_user_login_validation(self):
+        driver = self.driver
+
+        login = LoginPage(driver)
+        login.enter_username(self.Qc_username)
+        login.enter_password(self.Qc_password)
+        login.click_login()
+
+        time.sleep(4)
+
+
+    def test_12_Open_case_by_clicking(self):
+        driver = self.driver
+
+        homepage = HomePage(driver)
+        homepage.Open_case_by_clicking()
+        time.sleep(4)
+
+    def test_13_Verify_Case(self):
+        driver = self.driver
+
+        homepage = HomePage(driver)
+        homepage.Verify_case()
+        time.sleep(3)
+
+    def test_14_Logout_from_qc_user(self):
         driver = self.driver
 
         homepage = HomePage(driver)
@@ -70,3 +147,4 @@ class Bajaj_Claims_Test(unittest.TestCase):
 
 if __name__ == '__main__':
     unittest.main(testRunner=HtmlTestRunner.HTMLTestRunner(Locators.output))
+
