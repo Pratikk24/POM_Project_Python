3d76dedabdd5d7fbafeee6dce8e649cde9d35321
Pratikk24 <65650121+Pratikk24@users.noreply.github.com>
2020-07-08 13:20:22 +0530

    Add files via upload

--- Libraries/LIB_HomePage.py
+++ Libraries/LIB_HomePage.py
@@ -1,38 +1,63 @@
 from Locators.locators import Locators
 
-class HomePage():
+
+class Homepage():
 
     # Constructor
     def __init__(self, driver):
         self.driver = driver
 
-        self.Three_Dot_Icon = Locators.Three_Dot_Icon
-        self.Logout_x_path = Locators.logout_x_path
-        self.Document_list = Locators.Document_link_text
-        self.Upload_Document = Locators.Upload_Document_x_path
+        self.three_dot_icon = Locators.three_dot_icon
+        self.logout_x_path = Locators.logout_x_path
+        self.document_list = Locators.document_link_text
+        self.upload_document = Locators.upload_document_x_path
+        self.close_button = Locators.close_button_x_path
+        self.refresh_button = Locators.refresh_button_x_path
+        self.claims_list = Locators.claims_list_link_text
+        self.select_case = Locators.select_case
+        self.allocation_arrow = Locators.down_arrow_for_allocation
+        self.qc_user_selection = Locators.selection_of_qc_user
+        self.qc_user_allocation = Locators.allocation_of_qc_user
+        self.open_case = Locators.open_case_x_path
+        self.verify_button = Locators.verify_button_x_path
 
     # Functions or Action Methods
 
-    def click_on_Document_list(self):
-        self.driver.find_element_by_link_text(self.Document_list).click()
+    def click_on_document_list(self):
+        self.driver.find_element_by_link_text(self.document_list).click()
 
-    def click_on_Upload_Document(self):
-        self.driver.find_element_by_xpath(self.Upload_Document).click()
+    def click_on_upload_document(self):
+        self.driver.find_element_by_xpath(self.upload_document).click()
 
-    def click_Three_Dot_Icon(self):
-        self.driver.find_element_by_xpath(self.Three_Dot_Icon).click()
+    def click_on_close_button(self):
+        self.driver.find_element_by_xpath(self.close_button).click()
 
-    def click_on_Logout(self):
-        self.driver.find_element_by_xpath(self.Logout_x_path).click()
+    def click_on_refresh_button(self):
+        self.driver.find_element_by_xpath(self.refresh_button).click()
 
+    def click_on_claims_list(self):
+        self.driver.find_element_by_link_text(self.claims_list).click()
 
+    def click_on_select_case(self):
+        self.driver.find_element_by_xpath(self.select_case).click()
 
+    def click_on_allocation_arrow(self):
+        self.driver.find_element_by_xpath(self.allocation_arrow).click()
 
+    def click_on_qc_user_selection(self):
+        self.driver.find_element_by_xpath(self.qc_user_selection).click()
 
+    def click_on_qc_user_allocation(self):
+        self.driver.find_element_by_xpath(self.qc_user_allocation).click()
 
+    def open_case_by_clicking(self):
+        self.driver.find_element_by_xpath(self.open_case).click()
 
+    def verify_case(self):
+        self.driver.find_element_by_xpath(self.verify_button).click()
 
+    def click_on_three_dot_icon(self):
+        self.driver.find_element_by_xpath(self.three_dot_icon).click()
 
-
-
-
+    def click_on_logout(self):
+        self.driver.find_element_by_xpath(self.logout_x_path).click()
--- Locators/locators.py
+++ Locators/locators.py
@@ -4,7 +4,7 @@ class Locators():
     executable_path = "C:/Driver/chromedriver_win32/chromedriver.exe"
 
     # url :
-    URL = "http://claim-portal-bajaj-dev.qa.i3systems.in/"
+    url = "http://claim-portal-bajaj-dev.qa.i3systems.in/"
 
     # Login Page Objects :
     username_textbox_id = "id_username"
@@ -15,17 +15,41 @@ class Locators():
     Username = "admin"
     Password = "!3$y$+eMs"
 
+    # qc_username & Password :
+    qc_username = "qc_user1"
+    qc_password = "!3$y$+eMs"
 
     # Home Page Objects :-
 
     # Document Page Objects
-    Document_link_text = "DOCUMENT LIST"
+    document_link_text = "DOCUMENT LIST"
 
     # Upload Documents :
-    Upload_Document_x_path = "/html/body/app-root/section/main/div/app-document-list/section[1]/section/section[2]/section/button[1]"
+    upload_document_x_path = "/html/body/app-root/section/main/div/app-document-list/section[1]/section/section[2]/section/button[1]"
+
+    # Claim list page Objects :-
+    claims_list_link_text = "CLAIMS LIST"
+
+    # select case
+    select_case = "/html/body/app-root/section/main/div/app-claims-list/section[2]/section[1]/table/tbody/tr[1]/td[1]/mat-checkbox/label/div"
+
+    # down arrow for allocation
+    down_arrow_for_allocation = "/html/body/app-root/section/main/div/app-claims-list/section[1]/section/section[2]/section/mat-form-field/div/div[1]/div/mat-select/div/div[2]/div"
+
+    # selection of  qc user
+    selection_of_qc_user = "/html/body/div[2]/div[2]/div/div/div/mat-option[3]/span"
+
+    # allocation of qc user
+    allocation_of_qc_user = "/html/body/app-root/section/main/div/app-claims-list/section[1]/section/section[2]/section/button[1]/span/mat-icon"
+
+    # Click on Case
+    open_case_x_path = "//*[@id='i3tableId']/tbody/tr[1]"
+
+    # Verify_Button
+    verify_button_x_path = "/html/body/app-root/section/main/div/app-qc-page/app-qc-header/section/section/section[3]/button[2]"
 
     # Logout :
-    Three_Dot_Icon = "//*[@id='header-main']/app-header/mat-toolbar/div[2]/ul[2]/li[3]/button/span/mat-icon"
+    three_dot_icon = "//*[@id='header-main']/app-header/mat-toolbar/div[2]/ul[2]/li[3]/button/span/mat-icon"
     logout_x_path = "//*[@id='cdk-overlay-0']/div/div/button[2]/span"
 
     # Title
@@ -34,9 +58,26 @@ class Locators():
     # Report output Path
     output = 'C:/Users/Pratik/PycharmProjects/Claims-POM/Reports'
 
+    # Close Button
+    close_button_x_path = "//*[@id='file-upload-section']/app-material-file-upload/section/section[1]/button[2]/span/mat-icon"
 
+    # Refresh Button
+    refresh_button_x_path = "/html/body/app-root/section/main/div/app-document-list/section[1]/section/section[2]/section/button[3]"
 
+    # file name path
+    file_name_x_path = "//*[@id='i3tableId']/tbody/tr[1]/td[2]/section/span"
 
+    # file name
+    file_name = "o210328230-14.jpg"
 
+    # Case state
+    case_state_x_path = "//*[@id='i3tableId']/tbody/tr[1]/td[3]/section/span"
 
+    # state
+    state = "Case Created"
 
+    # Document Upload Successfully
+    upload_successfully_x_path = "/html/body/app-root/section/main/div/app-document-list/section[1]/section[2]/app-material-file-upload/section/section[2]/mat-list/mat-list-item/div/div[2]/p/span[3]"
+
+    # Upload successfully message
+    upload_successfully_text = "File o210328230-14.jpg uploaded successfully!"
