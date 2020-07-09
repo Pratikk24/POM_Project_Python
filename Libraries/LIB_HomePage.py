201049552d04b9843443ddecd2da44ced1179d6c
Pratikk24 <65650121+Pratikk24@users.noreply.github.com>
2020-07-09 19:18:31 +0530

    Add files via upload

--- Libraries/LIB_HomePage.py
+++ Libraries/LIB_HomePage.py
@@ -10,7 +10,6 @@ class Homepage():
         self.three_dot_icon = Locators.three_dot_icon
         self.logout_x_path = Locators.logout_x_path
         self.document_list = Locators.document_link_text
-        self.upload_document = Locators.upload_document_x_path
         self.close_button = Locators.close_button_x_path
         self.refresh_button = Locators.refresh_button_x_path
         self.claims_list = Locators.claims_list_link_text
@@ -19,18 +18,14 @@ class Homepage():
         self.qc_user_selection = Locators.selection_of_qc_user
         self.qc_user_allocation = Locators.allocation_of_qc_user
         self.open_case = Locators.open_case_x_path
+        self.all_doc_icon = Locators.all_doc_icon_x_path
+        self.back_arrow_button = Locators.back_arrow_x_path
         self.verify_button = Locators.verify_button_x_path
 
     # Functions or Action Methods
 
     def click_on_document_list(self):
         self.driver.find_element_by_link_text(self.document_list).click()
-
-    def click_on_upload_document(self):
-        self.driver.find_element_by_xpath(self.upload_document).click()
-
-    def click_on_close_button(self):
-        self.driver.find_element_by_xpath(self.close_button).click()
 
     def click_on_refresh_button(self):
         self.driver.find_element_by_xpath(self.refresh_button).click()
@@ -52,6 +47,16 @@ class Homepage():
 
     def open_case_by_clicking(self):
         self.driver.find_element_by_xpath(self.open_case).click()
+
+    def click_on_all_doc_icon(self):
+        self.driver.find_element_by_xpath(self.all_doc_icon).click()
+
+    def click_all_doc_tabs(self):
+        for i in range(1, 25):
+            self.driver.find_element_by_xpath(Locators.start+str(i)+Locators.end ).click()
+
+    def click_back_button(self):
+         self.driver.find_element_by_xpath(self.back_arrow_button).click()
 
     def verify_case(self):
         self.driver.find_element_by_xpath(self.verify_button).click()
--- Locators/locators.py
+++ Locators/locators.py
@@ -16,16 +16,13 @@ class Locators():
     Password = "!3$y$+eMs"
 
     # qc_username & Password :
-    qc_username = "qc_user1"
+    qc_username = "Ajit_QC_Bajaj"
     qc_password = "!3$y$+eMs"
 
     # Home Page Objects :-
 
     # Document Page Objects
     document_link_text = "DOCUMENT LIST"
-
-    # Upload Documents :
-    upload_document_x_path = "/html/body/app-root/section/main/div/app-document-list/section[1]/section/section[2]/section/button[1]"
 
     # Claim list page Objects :-
     claims_list_link_text = "CLAIMS LIST"
@@ -37,20 +34,20 @@ class Locators():
     down_arrow_for_allocation = "/html/body/app-root/section/main/div/app-claims-list/section[1]/section/section[2]/section/mat-form-field/div/div[1]/div/mat-select/div/div[2]/div"
 
     # selection of  qc user
-    selection_of_qc_user = "/html/body/div[2]/div[2]/div/div/div/mat-option[3]/span"
+    selection_of_qc_user = "/html/body/div[2]/div[2]/div/div/div/mat-option[2]/span"
 
     # allocation of qc user
     allocation_of_qc_user = "/html/body/app-root/section/main/div/app-claims-list/section[1]/section/section[2]/section/button[1]/span/mat-icon"
 
     # Click on Case
-    open_case_x_path = "//*[@id='i3tableId']/tbody/tr[1]"
+    open_case_x_path = "/html/body/app-root/section/main/div/app-claims-list/section[2]/section[1]/table/tbody/tr[1]"
 
     # Verify_Button
     verify_button_x_path = "/html/body/app-root/section/main/div/app-qc-page/app-qc-header/section/section/section[3]/button[2]"
 
     # Logout :
     three_dot_icon = "//*[@id='header-main']/app-header/mat-toolbar/div[2]/ul[2]/li[3]/button/span/mat-icon"
-    logout_x_path = "//*[@id='cdk-overlay-0']/div/div/button[2]/span"
+    logout_x_path = "/html/body/div[2]/div[2]/div/div/div/button[2]/span"
 
     # Title
     title = "Claim Assistant"
@@ -64,12 +61,6 @@ class Locators():
     # Refresh Button
     refresh_button_x_path = "/html/body/app-root/section/main/div/app-document-list/section[1]/section/section[2]/section/button[3]"
 
-    # file name path
-    file_name_x_path = "//*[@id='i3tableId']/tbody/tr[1]/td[2]/section/span"
-
-    # file name
-    file_name = "o210328230-14.jpg"
-
     # Case state
     case_state_x_path = "//*[@id='i3tableId']/tbody/tr[1]/td[3]/section/span"
 
@@ -81,3 +72,21 @@ class Locators():
 
     # Upload successfully message
     upload_successfully_text = "File o210328230-14.jpg uploaded successfully!"
+
+    #all doc icon
+    all_doc_icon_x_path = "/html/body/app-root/section/main/div/app-qc-page/section/section/section[1]/section/app-document-view-page/section[1]/section[2]/button[1]/span/mat-icon"
+
+    # all docs x path
+    start = "/html/body/app-root/section/main/div/app-all-documents/section[2]/section[1]/section/mat-list/mat-list-item["
+    end = "]/div/section/section[1]/h3/span"
+
+    # back arrow icon
+    back_arrow_x_path = "/html/body/app-root/section/main/div/app-all-documents/section[1]/section[1]/button[1]/span/mat-icon"
+
+
+
+
+
+
+
+
